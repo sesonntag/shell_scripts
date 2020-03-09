b57f18dfa11eaa37600e0a4cf6d5e87e156fd720
Sebastian Sonntag <se.bastian.sonntag@hotmail.com>
2020-03-09 19:07:13 +0100

    Update backup_home_to_synology.sh
    change user for ssh access since synology changed policy

--- backup_home_to_synology.sh
+++ backup_home_to_synology.sh
@@ -4,13 +4,13 @@
 # Title: backup_home_to_synology.sh
 # Description: copy files from users dir to Synology DiskStation NAS
 # Author: Sebastian Sonntag
-# Date: 2019-09-06
+# Date: 2020-03-03
 # License:
 #*******************************************************************************
 
 echo "*************************************************************************"
 echo "Syncing now: Sebastians home dir"
-rsync -a -h -x -v --info=progress2 --delete --copy-links --exclude-from='/Users/sebastiansonntag/iCloud/System/shell_scripts/rsync_exclude_in_home.txt' -e ssh ~/ sebastian@192.168.1.47:/volume1/homes/sebastian/backups/mac_book_pro/
+rsync -a -h -x -v --info=progress2 --delete --copy-links --exclude-from='/Users/sebastiansonntag/iCloud/System/shell_scripts/rsync_exclude_in_home.txt' -e ssh ~/ admin@192.168.1.47:/volume1/homes/sebastian/backups/mac_book_pro/
 echo "-------------------------------------------------------------------------"
 echo ""
 echo ""
