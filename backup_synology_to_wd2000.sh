e6b80507f91947d2b3dfb8a9b84c01619aa89116
Sebastian Sonntag <se.bastian.sonntag@hotmail.com>
2021-03-23 16:48:41 +0100

    add new location on NAS

--- backup_synology_to_wd2000.sh
+++ backup_synology_to_wd2000.sh
@@ -4,19 +4,19 @@
 # Title: rsync_synology_to_wd2000.sh
 # Description: backup files from Synology DiskStation to external hdd wd2000
 # Author: Sebastian Sonntag
-# Date: 2020-03-18
+# Date: 2020-07-02
 # License:
 #*******************************************************************************
 
 ### rsync commands to rsync each directory incl. all subdirectories #
 echo "*************************************************************************"
 echo "Syncing now:"
-echo "     /volume1/homes/sebastian"
+echo "     /volume1/homes/admin"
 echo "     /volume1/homes/laura"
 echo ""
 echo "-------------------------------------------------------------------------"
-echo "task: sebastians home dir on synology"
-rsync -h -a -H -x -r -v --info=progress2 --update --delete --exclude '@eaDir*' -e ssh admin@192.168.1.47:/volume1/homes/sebastian /Volumes/wd2000/backups_synology_ds218/
+echo "task: admin (sebastians) home dir on synology"
+rsync -h -a -H -x -r -v --info=progress2 --update --delete --exclude '@eaDir*' -e ssh admin@192.168.1.47:/volume1/homes/admin /Volumes/wd2000/backups_synology_ds218/
 echo "-------------------------------------------------------------------------"
 echo ""
 echo "-------------------------------------------------------------------------"
