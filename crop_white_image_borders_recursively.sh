cb2050eac464fa3f22cb4faca8182115dc5ac190
Sebastian Sonntag <sesonntag@users.noreply.github.com>
2026-03-11 14:21:38 +0100

    Update crop_white_image_borders_recursively.sh

--- crop_white_image_borders_recursively.sh
+++ crop_white_image_borders_recursively.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+#
+#*******************************************************************************
+# Title: crop_white_image_borders_recursively.shs.sh
+# Description:
+# Author: Sebastian Sonntag
+# Date: 2026-03-11
+# License: MIT
+#*******************************************************************************
+
 find Bilder -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) -exec sh -c '
   for f do
     magick "$f" -fuzz 10% -trim +repage -bordercolor white -border 5 "$f"
   done
+
 ' sh {} +
